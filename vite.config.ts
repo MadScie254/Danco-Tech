0ed35198290babefa8f3b8a939dc207552b9f57c
Daniel Wanjala <134260642+MadScie254@users.noreply.github.com>
2026-08-02 14:42:23 +0300

    Apply blocked performans file fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss(), cloudflare()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
